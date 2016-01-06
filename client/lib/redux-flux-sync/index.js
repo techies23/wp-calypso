86b7fae0e6423ecd4ef71899bd5a762b49c54b1c
Andrew Duthie <andrew@andrewduthie.com>
2016-01-06 15:52:48 -0500

    Framework: Improve reduxFluxSync documentation

--- client/lib/redux-flux-sync/index.js
+++ client/lib/redux-flux-sync/index.js
@@ -5,12 +5,22 @@ import Dispatcher from 'dispatcher';
 import { receivePost } from 'state/posts/actions';
 
 /**
- * Module variables
+ * Object mapping Flux action type to a function which, when called with the
+ * Flux action object, returns a Redux action object to be dispatched to the
+ * Redux store.
  */
 const SYNC_ACTIONS = {
 	RECEIVE_POST_TO_EDIT: ( action ) => receivePost( action.post )
 };
 
+/**
+ * Subscribes to the global Flux dispatcher and dispatches to the provided
+ * Redux store instance if an action mapping can be determined when a Flux
+ * action is dispatched.
+ *
+ * @param  {Object} store Redux store instance
+ * @return {String}       Dispatcher subscription ID
+ */
 export default function( store ) {
 	return Dispatcher.register( ( payload ) => {
 		let handler = SYNC_ACTIONS[ payload.action.type ];
