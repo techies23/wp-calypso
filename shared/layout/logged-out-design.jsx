c9a6a513c2605f6f848e6ed82239781f38dd6e1c
Miguel Fonseca <miguelcsf@gmail.com>
2016-01-12 21:40:06 +0000

    Add ThemesHead component

--- shared/layout/logged-out-design.jsx
+++ shared/layout/logged-out-design.jsx
@@ -7,15 +7,11 @@ import React from 'react';
  * Internal dependencies
  */
 import MasterbarLoggedOut from 'layout/masterbar/logged-out';
-import Head from 'layout/head';
+import ThemesHead from 'components/themes-head';
 
 const LayoutLoggedOutDesign = ( { tier = 'all' } ) => (
 	<div className="wp is-section-design has-no-sidebar">
-		<Head
-			title={ get( 'title', tier ) }
-			description={ get( 'description', tier ) }
-			canonicalUrl={ get( 'canonicalUrl', tier ) }
-		/>
+		<ThemesHead tier={ tier } />
 		<MasterbarLoggedOut />
 		<div id="content" className="wp-content">
 			<div id="primary" className="wp-primary wp-section" />
@@ -28,30 +24,6 @@ const LayoutLoggedOutDesign = ( { tier = 'all' } ) => (
 LayoutLoggedOutDesign.displayName = 'LayoutLoggedOutDesign';
 LayoutLoggedOutDesign.propTypes = {
 	tier: React.PropTypes.string
-}
-
-const designMeta = {
-	all: {
-		title: 'WordPress Themes',
-		description: 'Beautiful, responsive, free and premium WordPress themes for your photography site, portfolio, magazine, business website, or blog.',
-		canonicalUrl: 'https://wordpress.com/design',
-	},
-	free: {
-		title: 'Free WordPress Themes at WordPress.com',
-		description: 'Discover Free WordPress Themes on the WordPress.com Theme Showcase.',
-		canonicalUrl: 'https://wordpress.com/design/type/free',
-	},
-	premium: {
-		title: 'Premium WordPress Themes at WordPress.com',
-		description: 'Discover Premium WordPress Themes on the WordPress.com Theme Showcase.',
-		canonicalUrl: 'https://wordpress.com/design/type/premium',
-	}
-}
-
-function get( key, tier ) {
-	return tier in designMeta && key in designMeta[ tier ]
-	? designMeta[ tier ][ key ]
-	: '';
 }
 
 export default LayoutLoggedOutDesign;
